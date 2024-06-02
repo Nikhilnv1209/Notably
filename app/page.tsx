d7efd6a585f4142ff73b0e3cd15a865c61b9701b
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-02 22:00:34 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <p className="bg-black text-white h-full flex items-center justify-center">
+      This is the starting of the Notibly
+    </p>
+  );
+}
