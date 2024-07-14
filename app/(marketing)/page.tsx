deb7e0c188059ab8de8fe1f3680de01a8b2f12a7
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-07-14 21:30:29 +0530

    Feat(LandingPage): Integrate Hero component
    Added Hero component to the landing page component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/Heading";
+import Heroes from "./_components/Heroes";
 
 export default function MarketingPage() {
   return (
     <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-4 md:px-14 sm:px-6 pb-10">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-4 flex-1 px-4 md:px-14 sm:px-6 pb-10">
         <Heading/>
+        <Heroes/>
       </div>
     </div>
   );
